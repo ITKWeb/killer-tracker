fe246c90941b31bc0733bf635a345bb4cbb7c477
Michele Scapin <michele.scapin@itkweb.com>
2014-10-03 15:59:40 +0200

    launchGame #1

--- webapp/killerHD-client-ng/app/scripts/controllers/launchGame.js
+++ webapp/killerHD-client-ng/app/scripts/controllers/launchGame.js
@@ -8,6 +8,26 @@
  * Controller of the killerTrackerNgApp
  */
 angular.module('killerTrackerNgApp')
-  .controller('LaunchGameCtrl', function ($scope) {
+  .controller('LaunchGameCtrl', ['$scope', function ($scope) {
+    
     $scope.title = 'Launch Game';
-  });
+
+    $scope.players = [{}];
+    
+    console.log($scope.players);
+
+    $scope.add = function(player) {
+    	//console.log(newplayer.email);
+    	player.isAdded = true;
+    	$scope.players.push({}); 
+    	console.log($scope.players);
+    };
+
+    $scope.remove = function(index) {
+    	
+    	console.log('remove ' , index);
+    	$scope.players.splice(index,1);
+		
+    };
+
+  }]);
